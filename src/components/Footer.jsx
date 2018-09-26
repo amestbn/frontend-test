bd51dbc40c7c4a4c9c31dd5a6ae5dc67926cbe11
Aura Esteban <auraesteban@gmail.com>
2018-09-27 00:44:35 +0800

    Fix total count

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 
 const mapStateToProps = state => {
   return {
-    total: state.items.length
+    total: state.items.reduce((accumulator, value) => accumulator + value.count, 0)
   }
 };
 
@@ -11,8 +11,9 @@ class Footer extends Component {
   render() {
     const { total } = this.props;
     return(
-      <div>
-        Total: { total }
+      <div className="total">
+        <span>Total:</span>
+        <span>{ total }</span>
       </div>
     );
   }
